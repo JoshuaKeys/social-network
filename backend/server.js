958a0dc8b098582aab207433914a8fa9278d21ed
Joshua <joshua_oguma@epam.com>
2021-05-10 11:56:22 +0300

    added continous deployment

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const http = require('http');
 
 const app = express();
 
 app.use(express.static('dist'));
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+const server = http.createServer(app);
+
+server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+module.exports = server;
--- backend/test/test.js
+++ backend/test/test.js
@@ -1,9 +1,10 @@
 var assert = require('assert');
-
+var server = require('../server.js');
 describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
       assert.equal([1, 2, 3].indexOf(4), -1);
+      server.close();
     });
   });
 });
